bbc691d9b91fa0223fab4380bafdf906b6fe5df4
Mrdeilen <321080893@qq.com>
2019-05-24 16:11:27 +0800

    在吗

--- js/index.js
+++ js/index.js
@@ -5,6 +5,11 @@ var timerID = setInterval(function () {
     i++
     alert('我是Deilen')
 
+    if (confirm('在吗')) {
+        alert('嘻嘻嘻')
+    } else {
+        alert('看什么呢')
+    }
     if (i == 1) {
         clearInterval(timerID)
     }
